6bb9bd7c52401b52fc928a042857d0aa6c4b2963
KIPE_san <KIPE_san@DESKTOP-3DD0KCD>
2024-03-23 15:47:11 -0400

    Created class "Proyecto".

---
+++ ProyectoFinal/src/logico/Proyecto.java
@@ -0,0 +1,44 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Proyecto {
+
+	private String id;
+	private String nombre;
+	private ArrayList<Trabajador> losTrabajadores;
+	
+	public Proyecto(String id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.losTrabajadores = new ArrayList<>();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public ArrayList<Trabajador> getLosTrabajadores() {
+		return losTrabajadores;
+	}
+
+	public void setLosTrabajadores(ArrayList<Trabajador> losTrabajadores) {
+
+		this.losTrabajadores = losTrabajadores; //hacer exepcion o validacion para no aceptar < 4 y > 5
+
+	}
+	
+}
